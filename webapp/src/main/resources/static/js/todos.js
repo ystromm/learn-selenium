fe2f62e019ad5fe69f30aa0e805d1345253a9e63
Martin Carlsson <martin.x.carlsson@arbetsformedlingen.se>
2016-10-24 11:57:49 +0200

    Extracted view to separate file.

--- webapp/src/main/resources/static/js/todos.js
+++ webapp/src/main/resources/static/js/todos.js
@@ -1,7 +1,39 @@
 (function () {
     "use strict";
-    angular.module("todos", [])
-        .controller("todos", function ($scope) {
-            $scope.todos = [{id: 1, text: "Remember this!", done: false}, {id: 2, text: "Fuhgettaboutit!", done: true}];
+    angular.module("todos", ['ui.router'])
+        .config(function ($stateProvider, $urlRouterProvider) {
+
+            $urlRouterProvider.otherwise('/');
+
+            $stateProvider
+                // HOME STATES AND NESTED VIEWS ========================================
+                .state('home', {
+                    url: '/',
+                    templateUrl: 'todos.html'
+                })
+                // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
+                .state('about', {
+                    // we'll get to this in a bit
+                });
+        })
+        .controller("todos", function ($scope, TodosResource) {
+            $scope.todos = [];
+            TodosResource.query().$promise.then(function (todos) {
+                $scope.todos = todos;
+                $scope.error = undefined;
+            }, function (error) {
+                $scope.error = error;
+            });
+            $scope.add = function () {
+                $scope.todos.push({id: 0, text: $scope.text, done: false});
+            };
+            $scope.done = function (todo) {
+                todo.done = !todo.done;
+            };
+        }).factory('TodosResource', function ($resource, TODOS_URL) {
+        return $resource(TODOS_URL + '/api/todo/:id', {id: '@id'}, {
+            update: {method: 'PUT'}
         });
+    });
+    ;
 }());
