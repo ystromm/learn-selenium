9ac900be03a3ba84f21fe8dc02d1e186d8e2dc32
Martin Carlsson <martin.x.carlsson@arbetsformedlingen.se>
2016-10-26 11:43:57 +0200

    Tried switching to firefox...

--- webapp/src/test/java/com/github/ystromm/learn_selenium/webapp/AddTodoTest.java
+++ webapp/src/test/java/com/github/ystromm/learn_selenium/webapp/AddTodoTest.java
@@ -1,6 +1,7 @@
 package com.github.ystromm.learn_selenium.webapp;
 
 import com.github.ystromm.learn_selenium.backend_impl.BackendMain;
+import com.google.common.base.Charsets;
 import org.awaitility.Duration;
 import org.junit.After;
 import org.junit.Before;
@@ -11,13 +12,18 @@ import org.junit.runner.RunWith;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
 import org.springframework.boot.context.embedded.LocalServerPort;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import static org.awaitility.Awaitility.await;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -36,10 +42,25 @@ public class AddTodoTest {
     private WebDriver webDriver;
 
     @Before
-    public void openBrowser() {
+    public void openBrowser() throws IOException {
         System.setProperty("webdriver.chrome.driver", "./chromedriver.exe");
-        webDriver = new ChromeDriver();
+        final FirefoxBinary firefoxBinary = onTravis() ? new FirefoxBinary(new File(getFirefoxPath())): new FirefoxBinary();
+        webDriver = new FirefoxDriver(firefoxBinary, new FirefoxProfile());
         webDriver.get("http://localhost:" + localServerPort);
+    }
+
+    private static boolean onTravis() {
+        return "true".equals(System.getenv().get("TRAVIS"));
+    }
+
+    private static String getFirefoxPath() throws IOException {
+        final ProcessBuilder processBuilder = new ProcessBuilder("which", "firefox");
+        processBuilder.redirectErrorStream(true);
+        final Process process = processBuilder.start();
+        try (final InputStreamReader inputStreamReader = new InputStreamReader(process.getInputStream(), Charsets.UTF_8);
+             final BufferedReader bufferedReader = new BufferedReader(inputStreamReader)) {
+            return bufferedReader.readLine();
+        }
     }
 
     @After
