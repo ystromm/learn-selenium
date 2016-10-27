f551e29f1fdadce68830ff7b6bde129d072795a2
Martin Carlsson <martin.x.carlsson@arbetsformedlingen.se>
2016-10-27 09:32:22 +0200

    Ignored schreenshots.

--- webapp/src/test/java/com/github/ystromm/learn_selenium/webapp/DoneTodoTest.java
+++ webapp/src/test/java/com/github/ystromm/learn_selenium/webapp/DoneTodoTest.java
@@ -15,17 +15,18 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
+import java.util.List;
 
 import static com.github.ystromm.learn_selenium.webapp.Screenshot.screenshot;
 import static org.awaitility.Awaitility.await;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.isEmptyString;
+import static org.hamcrest.Matchers.hasSize;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {WebappMain.class, BackendMain.class}, webEnvironment = RANDOM_PORT)
-public class UpdateTodoTest {
+public class DoneTodoTest {
     @Rule
     public TestName testName = new TestName();
     @LocalServerPort
@@ -34,6 +35,7 @@ public class UpdateTodoTest {
 
     @Before
     public void openBrowser() throws IOException {
+        // System.setProperty("webdriver.chrome.driver", "./chromedriver.exe");
         webDriver = Firefox.firefoxDriver();
         webDriver.get("http://localhost:" + localServerPort);
     }
@@ -45,26 +47,34 @@ public class UpdateTodoTest {
     }
 
     @Test
-    public void title_should_be_todos() {
-        assertThat(webDriver.getTitle(), equalTo("Todos"));
+    public void check_done_should_toggle_checkbox() {
+        addTodoItem();
+        final List<WebElement> todos_items_item_done_checkbox_before = webDriver.findElements(Bys.byTestId("todos_items_item_done_checkbox"));
+        final boolean checkedBefore = Boolean.parseBoolean(todos_items_item_done_checkbox_before.get(0).getAttribute("checked"));
+        todos_items_item_done_checkbox_before.get(0).click();
+        await().atMost(Duration.ONE_SECOND).until(() -> {
+                    final List<WebElement> todos_items_item_done_checkbox_after = webDriver.findElements(Bys.byTestId("todos_items_item_done_checkbox"));
+                    final boolean checkedAfter = Boolean.parseBoolean(todos_items_item_done_checkbox_after.get(0).getAttribute("checked"));
+                    assertThat(checkedBefore, equalTo(!checkedAfter));
+                }
+        );
     }
 
     @Test
-    public void heading_should_be_todos() {
-        assertThat(webDriver.findElement(Bys.byTestId("todos_heading")).getText(), equalTo("Todos"));
+    public void check_done_should_not_add_items() {
+        addTodoItem();
+        final List<WebElement> todos_items_item_done_checkbox_before = webDriver.findElements(Bys.byTestId("todos_items_item_done_checkbox"));
+        todos_items_item_done_checkbox_before.get(0).click();
+        await().atMost(Duration.ONE_SECOND).until(() -> {
+                    assertThat(webDriver.findElements(Bys.byTestId("todos_items_item_done_checkbox")), hasSize(todos_items_item_done_checkbox_before.size()));
+                }
+        );
     }
 
-    @Test
-    public void error_should_not_be_displayed() {
-        assertThat(webDriver.findElement(Bys.byTestId("todos_error")).isDisplayed(), equalTo(false));
-    }
-
-    @Test
-    public void add_should_clear_text() {
+    private void addTodoItem() {
         final WebElement todos_add_input = webDriver.findElement(Bys.byTestId("todos_add_input"));
         final WebElement todos_add_button = webDriver.findElement(Bys.byTestId("todos_add_button"));
         todos_add_input.sendKeys("To do item");
         todos_add_button.click();
-        await().atMost(Duration.ONE_SECOND).until(() -> assertThat(todos_add_input.getAttribute("value"), isEmptyString()));
     }
 }
