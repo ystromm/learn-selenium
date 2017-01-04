7576d478e1869e33740dd0c5c642f75bebd9f835
Martin Carlsson <martin.carlsson@omegapoint.se>
2017-01-04 12:36:58 +0100

    Bundled backend with frontend.

--- webapp-elm/src/main/elm/App.elm
+++ webapp-elm/src/main/elm/App.elm
@@ -3,11 +3,47 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 --import Html.Events.Extra exposing (..)
 import String
-import Http
+import Http exposing (...)
 import Json.Decode as Decode
 import Task exposing (..)
+
+type alias Todo =
+  { text : String
+  , id : Int,
+  done: Bool
+  }
+
+getTodos : Http.Request List Todo
+getTodos =
+  Http.get "http://localhost:8080/api/todos decodeTodos
+
+decodeTodos: Decode.Decoder List Todo
+decodeTodos = Decode.map2 Todo
+    (Decode.field "done" Decode.boolean)
+    (Decode.field "text" Decode.string)
+    (Decode.field "id" Decode.int)
+
+    getRandomGif : String -> Cmd Msg
+    getRandomGif topic =
+      let
+        url =
+          "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" ++ topic
+
+        request =
+          Http.get url decodeGifUrl
+      in
+        Http.send NewGif request
+
+    decodeGifUrl : Json.Decoder String
+    decodeGifUrl =
+      Json.at ["data", "image_url"] Json.string
+
+--init
 
 main =
         ul [attribute "data-test-id" "todos_items_ul"] [
            li [attribute "data-test-id" "todos_items_li"] [text "Don't forget!"]
         ]
+
+
+--view
--- webapp-elm/src/main/java/com/github/ystromm/learn_selenium/webapp_elm/WebappMain.java
+++ webapp-elm/src/main/java/com/github/ystromm/learn_selenium/webapp_elm/WebappMain.java
@@ -1,12 +1,15 @@
 package com.github.ystromm.learn_selenium.webapp_elm;
 
+import com.github.ystromm.learn_selenium.backend_impl.BackendMain;
+import com.github.ystromm.learn_selenium.backend_impl.TodoApiConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 public class WebappMain {
 
     public static void main(String[] args) {
-        SpringApplication.run(WebappMain.class, args);
+        SpringApplication.run(WebappMain.class,  args);
     }
 }
