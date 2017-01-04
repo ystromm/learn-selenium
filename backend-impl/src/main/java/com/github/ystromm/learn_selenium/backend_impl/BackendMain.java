2f6ff3a3e73ea7ec26a59e65e4162bd22e3aae51
Martin Carlsson <martin.carlsson@omegapoint.se>
2017-01-04 12:32:51 +0100

    Extracted separate configuration class for the backend.

--- backend-impl/src/main/java/com/github/ystromm/learn_selenium/backend_impl/BackendMain.java
+++ backend-impl/src/main/java/com/github/ystromm/learn_selenium/backend_impl/BackendMain.java
@@ -3,9 +3,8 @@ package com.github.ystromm.learn_selenium.backend_impl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
 public class BackendMain {
     public static void main(String[] args) {
-        SpringApplication.run(BackendMain.class, args);
+        SpringApplication.run(TodoApiConfiguration.class, args);
     }
 }
---
+++ backend-impl/src/main/java/com/github/ystromm/learn_selenium/backend_impl/TodoApiConfiguration.java
@@ -0,0 +1,7 @@
+package com.github.ystromm.learn_selenium.backend_impl;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApiConfiguration {
+}
