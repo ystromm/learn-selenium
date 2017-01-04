21feb9ac107ba80a8a288ee020bdcc8be6ff95c7
Martin Carlsson <martin.carlsson@omegapoint.se>
2017-01-04 17:29:23 +0100

    Rendered a list of hardcoded todos.

--- webapp-elm/src/main/elm/App.elm
+++ webapp-elm/src/main/elm/App.elm
@@ -3,47 +3,86 @@ import Html.Attributes exposing (..)
 import Html.Events exposing (..)
 --import Html.Events.Extra exposing (..)
 import String
-import Http exposing (...)
+--import Http exposing (...)
 import Json.Decode as Decode
 import Task exposing (..)
 
+
 type alias Todo =
-  { text : String
-  , id : Int,
+  { id: Int,
+  text : String,
   done: Bool
   }
 
-getTodos : Http.Request List Todo
-getTodos =
-  Http.get "http://localhost:8080/api/todos decodeTodos
+type alias Model =
+  {
+      todos: List Todo
+  }
 
-decodeTodos: Decode.Decoder List Todo
-decodeTodos = Decode.map2 Todo
-    (Decode.field "done" Decode.boolean)
-    (Decode.field "text" Decode.string)
-    (Decode.field "id" Decode.int)
-
-    getRandomGif : String -> Cmd Msg
-    getRandomGif topic =
-      let
-        url =
-          "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" ++ topic
-
-        request =
-          Http.get url decodeGifUrl
-      in
-        Http.send NewGif request
-
-    decodeGifUrl : Json.Decoder String
-    decodeGifUrl =
-      Json.at ["data", "image_url"] Json.string
-
---init
+type Msg
+  = MorePlease
+--  | NewGif (Result Http.Error String)
 
 main =
-        ul [attribute "data-test-id" "todos_items_ul"] [
-           li [attribute "data-test-id" "todos_items_li"] [text "Don't forget!"]
-        ]
+  Html.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
 
+init : (Model, Cmd Msg )
+init = (Model [ Todo 2 "Fuhgettaboutit" False, Todo 1 "Don't forget" True ] , Cmd.none )
 
---view
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    MorePlease ->
+      (model, Cmd.none)
+
+--    NewGif (Ok newUrl) ->
+--      (model Cmd.none)
+
+--    NewGif (Err _) ->
+--      (model, Cmd.none)
+
+view : Model -> Html Msg
+view model =
+        div [attribute "data-test-id" "todos_items_ul"]
+           (List.map (\ todo ->
+               div [attribute "data-test-id" "todos_items_li"] [
+                       input [ type_ "checkbox", checked todo.done] [],
+                       input [ type_ "text", value todo.text] [],
+                       button [ class "btn btn-default glyphicon glyphicon-remove" ] []
+                       ]
+           ) model.todos)
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+-- getTodos : Http.Request List Todo
+-- getTodos =
+--  Http.get "http://localhost:8080/api/todos decodeTodos
+
+--decodeTodos: Decode.Decoder List Todo
+--decodeTodos = Decode.map2 Todo
+--    (Decode.field "done" Decode.boolean)
+--    (Decode.field "text" Decode.string)
+--    (Decode.field "id" Decode.int)
+
+--    getRandomGif : String -> Cmd Msg
+--    getRandomGif topic =
+--      let
+--        url =
+--          "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" ++ topic
+
+--        request =
+--          Http.get url decodeGifUrl
+--      in
+--        Http.send NewGif request
+
+--    decodeGifUrl : Json.Decoder String
+--    decodeGifUrl =
+--      Json.at ["data", "image_url"] Json.string
+
