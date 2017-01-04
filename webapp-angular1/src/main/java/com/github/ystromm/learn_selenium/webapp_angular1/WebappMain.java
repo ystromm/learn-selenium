77bbe7167d6c3ea6d6560638242dca69e7c40345
Martin Carlsson <martin.carlsson@omegapoint.se>
2017-01-04 12:36:33 +0100

    Changed webapp to include backend

--- webapp-angular1/src/main/java/com/github/ystromm/learn_selenium/webapp_angular1/WebappMain.java
+++ webapp-angular1/src/main/java/com/github/ystromm/learn_selenium/webapp_angular1/WebappMain.java
@@ -1,9 +1,12 @@
 package com.github.ystromm.learn_selenium.webapp_angular1;
 
+import com.github.ystromm.learn_selenium.backend_impl.TodoApiConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import(TodoApiConfiguration.class)
 public class WebappMain {
     public static void main(String[] args) {
         SpringApplication.run(WebappMain.class, args);
